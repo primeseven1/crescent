ed6617cde2ca5fb5f7d945057a64a2434c4b5796
Christopher Fandrich <cgfcubing@gmail.com>
2024-03-03 01:02:43 -0800

    Remove b/w/l prefix from the inline assembly inputs/outputs in ioports

--- include/crescent/cpu/ioports.h
+++ include/crescent/cpu/ioports.h
@@ -4,37 +4,37 @@
 
 static inline void outportb(u16 port, u8 val)
 {
-    asm volatile("outb %b0, %w1" : : "a"(val), "Nd"(port) : "memory");
+    asm volatile("outb %0, %1" : : "a"(val), "Nd"(port) : "memory");
 }
 
 static inline void outportw(u16 port, u16 val)
 {
-    asm volatile("outw %w0, %w1" : : "a"(val), "Nd"(port) : "memory");
+    asm volatile("outw %0, %1" : : "a"(val), "Nd"(port) : "memory");
 }
 
 static inline void outportl(u16 port, u32 val)
 {
-    asm volatile("outl %l0, %w1" : : "a"(val), "Nd"(port) : "memory");
+    asm volatile("outl %0, %1" : : "a"(val), "Nd"(port) : "memory");
 }
 
 static inline u8 inportb(u16 port)
 {
     u8 ret;
-    asm volatile("inb %w1, %b0" : "=a"(ret) : "Nd"(port) : "memory");
+    asm volatile("inb %1, %0" : "=a"(ret) : "Nd"(port) : "memory");
     return ret;
 }
 
 static inline u16 inportw(u16 port)
 {
     u16 ret;
-    asm volatile("inw %w1, %w0" : "=a"(ret) : "Nd"(port) : "memory");
+    asm volatile("inw %1, %0" : "=a"(ret) : "Nd"(port) : "memory");
     return ret;
 }
 
 static inline u32 inportl(u16 port)
 {
     u32 ret;
-    asm volatile("inl %w1, %l0" : "=a"(ret) : "Nd"(port) : "memory");
+    asm volatile("inl %1, %0" : "=a"(ret) : "Nd"(port) : "memory");
     return ret;
 }
 
